3e25e07454e7f4091b9fcede0125e7b365e6887d
张岩 <zhangyan22@le.com>
2017-10-26 20:49:43 +0800

    Initial commit

---
+++ app/src/main/java/tk/thinkerzhangyan/viewpractice/MainActivity.java
@@ -0,0 +1,18 @@
+package tk.thinkerzhangyan.viewpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+//        ImageView imageView = (ImageView) findViewById(R.id.iv_bitmap);
+//        imageView.setImageDrawable(getDrawable(R.drawable.bitmapxml2));
+        setContentView(R.layout.activity_main);
+
+    }
+}
